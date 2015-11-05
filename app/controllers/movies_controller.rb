3ee677cf1e1a740f263b61709b63606eece60ae7
Cody Peterson <ccp4314@gmail.com>
2015-11-05 03:53:36 +0000

    haml and movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,8 +11,11 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+   @movies = Movie.order(params[:sort])
   end
+  
+
+  
 
   def new
     # default: render 'new' template
